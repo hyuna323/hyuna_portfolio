be8ed95e3016bdbefb1975e507f697da5a58e649
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-09 04:30:43 +0900

    휠 이벤트 적용

--- js/src/portfolio_main.js
+++ js/src/portfolio_main.js
@@ -1,30 +1,10 @@
-window.addEventListener('scroll', function() {
-  const posY = this.window.pageYOffset;
-  const portfolio = this.document.querySelector('#portfolio').getBoundingClientRect().top;
-  const about = this.document.querySelector('#about').getBoundingClientRect().top;
-  const skills = this.document.querySelector('#skills').getBoundingClientRect().top;
-  const project = this.document.querySelector('#project').getBoundingClientRect().top;
-  const contact = this.document.querySelector('#contact').getBoundingClientRect().top;
+$(function(){
 
-  const portfolioTop = posY + portfolio;
-  const aboutTop = posY + about;
-  const skillsTop = posY + skills;
-  const projectTop = posY + project;
-  const contactTop = posY + contact;
-
-  let totalHeight = document.body.scrollHeight - this.window.innerHeight -1;
-
-  if(posY >= portfolioTop && posY < aboutTop) {
-
-  } else if(posY >= aboutTop && posY < skillsTop) {
-
-  } else if(posY >= skillsTop && posY < projectTop) {
-
-  } else if(posY >= projectTop && posY <= contactTop) {
-
-  } else if(posY >= contactTop && posY <= totalHeight) {
-
-  } else {
-
-  }
-});
+  $('#fullpage').fullpage({
+		//options here
+    anchors: ['portfolio', 'about', 'skills', 'project', 'contact'],
+		autoScrolling:true,
+    navigation:true,
+    navigationPosition:'right'
+	});
+});       
