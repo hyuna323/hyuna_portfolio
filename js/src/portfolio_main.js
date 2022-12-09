6d59d9d405b365842a50ab82f0d08279fb593883
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-10 06:20:24 +0900

    project

--- js/src/portfolio_main.js
+++ js/src/portfolio_main.js
@@ -7,4 +7,25 @@ $(function(){
     navigation:true,
     navigationPosition:'right'
 	});
-});       
+});
+
+// modal --------------------------
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".modal_overlay");
+const closeBtn = modal.querySelector("button");
+const openModal = () => {
+  modal.classList.remove("on");
+}
+const colseModal = () => {
+  modal.classList.add("on");
+}
+overlay.addEventListener("click", colseModal);
+closeBtn.addEventListener("click", colseModal);
+openButton.addEventListener("click", openModal);
+
+// page move -----------------------------
+function home(){
+  let url = "https://hyunao.github.io/ediya_project/html/01_ediya_main.html";
+  window.location = url;
+}
