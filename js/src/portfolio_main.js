3fc44036be3622cc1d04cd9e28dbf3f0262a3462
hyunao <89890370+hyunao@users.noreply.github.com>
2022-12-28 06:11:01 +0900

    1920px 반응형

--- js/src/portfolio_main.js
+++ js/src/portfolio_main.js
@@ -30,5 +30,19 @@ function home(){
   window.location = url;
 }
 
+// let observer = new IntersectionObserver((e)=>{
+//   e.forEach((박스)=>{
+//     if (박스.isIntersecting){
+//       박스.target.style.opacity = 1;
+//     } else {
+//       박스.target.style.opacity = 0;
+//     }
+//   })
+// })
 
+// let section = document.querySelectorAll('section')
+// observer.observe(section[2])
+
+// const trigger = new ScrollTrigger.default();
+// trigger.add('[data-trigger]');
 
