86476bd9f8f48c226a1508094501543806af5759
hyunao <89890370+hyuna323@users.noreply.github.com>
2023-05-16 05:52:05 +0900

    자바스크립트 재적용
    이해를 위해 한번 더 공부하기 위해 지우고 다시 해봄

--- js/src/portfolio_main.js
+++ js/src/portfolio_main.js
@@ -120,25 +120,26 @@ function ofproject(){
 
 
   (function() {
-  
+    
     var controller = new ScrollMagic.Controller();
     var scene1 = new ScrollMagic.Scene({
-        triggerElement: ".project_list", //트리거 설정
-        triggerHook: 0.8
-      })
-    
+      triggerElement: ".project_list",
+      triggerHook: 0.8
+    })
+
   var revealElements = document.getElementsByClassName("project_list_item");
-  for (var i=0; i<revealElements.length; i++) {
-    
+  for (var i = 0; i < revealElements.length; i++) {
+
     var scene2 = new ScrollMagic.Scene({
       triggerElement: revealElements[i],
       offset: 50,
       triggerHook: 0.9
     })
-    .setClassToggle(revealElements[i], "visible") // add class toggle
+    .setClassToggle(revealElements[i], "visible")
     .addTo(controller)
-    // .addIndicators({name: "(box) " + (i+1), colorStart:"#F6B352", colorTrigger:"#F6B352"});
   }
-  
+
   }())
+
+
 
